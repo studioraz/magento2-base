2e33488f1f1d4432ecd894c1441a85ee60d73003
Mykhailo Yevlentiev <m.yevlentiev@ism.nl>
2023-02-22 14:28:21 +0200

    M2BAS-2 Add XML layout update by Attribute Set ID - wrote plugin

---
+++ Plugin/Catalog/Helper/Product/View.php
@@ -0,0 +1,21 @@
+<?php
+namespace SR\Base\Plugin\Catalog\Helper\Product;
+
+use Magento\Catalog\Model\Product;
+use Magento\Framework\View\Result\Page as ResultPage;
+
+class View
+{
+    public function beforeInitProductLayout(
+        \Magento\Catalog\Helper\Product\View $subject,
+        ResultPage $resultPage,
+        Product $product,
+                                             $params = null
+    ) {
+        $resultPage->addPageLayoutHandles(
+            ['attribute_set_id' => $product->getAttributeSetId()]
+        );
+
+        return [$resultPage, $product, $params];
+    }
+}
